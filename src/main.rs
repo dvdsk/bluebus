3099186634ce5c5d8939967b222fff93c0d142d3
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2020-05-10 14:26:08 +0200

    bluez based mapping

--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,136 @@
-use std::convert::TryInto;
-use std::str::FromStr;
-use std::sync::mpsc;
-use std::thread;
+use std::fs::File;
+use std::num::NonZeroU8;
 use std::time::Duration;
 
-use btleplug::api::{BDAddr, Central, Peripheral, ValueNotification, UUID};
-use btleplug::bluez::{adapter::ConnectedAdapter, manager::Manager};
-use simplelog::{SimpleLogger, LevelFilter, Config, ConfigBuilder};
+use rustbus::{get_system_bus_path, MessageBuilder, Conn, RpcConn, standard_messages};
+mod experiments;
 
-fn get_central(manager: &Manager) -> ConnectedAdapter {
-    let adapters = manager.adapters().unwrap();
-    let adapter = adapters.into_iter().nth(0).unwrap();
-    adapter.connect().unwrap()
+//idea:
+// -simple, no auth or pairing supported
+// no need to explicitly connect
+// builder pattern, by default pick first adapter (other is extra feature)
+// use "from_raw_fd" for File (write and notify)
+// store opened files in self so we close them on drop
+// builder pattern for all operations
+
+// extra ideas :
+// (safety) disconnect all connected on drop [make builder option?] 
+
+#[derive(Debug)]
+enum Error {
+    DbusConnectionError(rustbus::client_conn::Error),
+    CouldNotConnect,
 }
 
-const BASE_UUID: [u8;16] = [
-    131, 20, 153, 220, 231, 245, 91, 152, 153, 21, 183, 27, 175, 191, 112, 147
-];
-const TARGET_DEV: BDAddr = BDAddr {
-    address: [0x4C, 0x2C, 0xAC, 0x56, 0x46, 0xC6],
-}; //reverse of what is normally displayed
-
-//matches the nrf api approch
-fn char_uuid_from(base: [u8; 16], service: u16) -> UUID {
-    let mut final_uuid = [0u8; 16];
-    final_uuid = base;
-    //let service = service.to_be_bytes();
-    let service = service.to_le_bytes();
-    final_uuid[12] = service[0];
-    final_uuid[13] = service[1];
-    UUID::B128(final_uuid)
+impl From<rustbus::client_conn::Error> for Error {
+    fn from(err: rustbus::client_conn::Error) -> Error {
+        Error::DbusConnectionError(err)
+    }
+}
+impl<'a> From<rustbus::message::Message<'a,'a>> for Error {
+    fn from(_: rustbus::message::Message<'a,'a>) -> Error {
+        Error::CouldNotConnect
+    }
 }
 
-pub fn handle_notify(notification: ValueNotification, tx: mpsc::Sender<Vec<u8>>) {
-    dbg!("handling notify");
-    tx.send(notification.value).unwrap();
+struct BleBuilder<'a> {
+    connection: RpcConn<'a,'a>,
+    adapter_numb: u8,
 }
 
-fn read_be_u32(input: &mut &[u8]) -> u32 {
-    let (int_bytes, rest) = input.split_at(std::mem::size_of::<u32>());
-    *input = rest;
-    u32::from_be_bytes(int_bytes.try_into().unwrap())
+impl<'a> BleBuilder<'a> {
+    pub fn new() -> Result<Self, Error> {
+        let session_path = get_system_bus_path()?;
+        let con = Conn::connect_to_bus(session_path, true)?;
+        let mut connection = RpcConn::new(con);
+        // send the obligatory hello message
+        connection.send_message(&mut standard_messages::hello(), None)?;
+
+        Ok(BleBuilder {
+            connection,
+            adapter_numb: 0,
+        })
+    }
+
+    pub fn build(self) -> Result<Ble<'a>, Error> {
+        let BleBuilder{connection, adapter_numb} = self;
+        
+        Ok( Ble {
+            connection,
+            adapter_numb,
+            notify_handles: Vec::new(),
+        })
+    }
 }
 
-pub fn main() {
+struct Ble<'a> {
+    //adapter
+    connection: RpcConn<'a,'a>,
+    adapter_numb: u8,
+    notify_handles: Vec<File>,  
+}
 
-    let config = ConfigBuilder::new()
-        .add_filter_ignore_str("btleplug::bluez::adapter")
-        .build();
-    let _ = SimpleLogger::init(LevelFilter::Trace, config);
+impl<'a> Ble<'a> {
+    pub fn connect<T: Into<String>>(&mut self, adress: T) -> Result<(), Error> {
+        let adress =adress.into().replace(":","_");
 
-    let (tx, rx) = mpsc::channel();
-    let manager = Manager::new().unwrap();
+        let mut connect = MessageBuilder::new()
+            .call("Connect".into())
+            .at("org.bluez".into())
+            .on(format!("/org/bluez/hci{}/dev_{}",self.adapter_numb, adress))
+            .with_interface("org.bluez.Device1".into()) //is always Device1
+            .build();
 
-    // get the first bluetooth adapter
-    let adapters = manager.adapters().unwrap();
-    let mut adapter = adapters.into_iter().nth(0).unwrap();
-
-    // reset the adapter -- clears out any errant state
-    adapter = manager.down(&adapter).unwrap();
-    adapter = manager.up(&adapter).unwrap();
-
-    // connect to the adapter
-    let central = adapter.connect().unwrap();
-    dbg!("connected to adapter");
-
-    // start scanning for devices
-    central.start_scan().unwrap();
-    // instead of waiting, you can use central.on_event to be notified of
-    // new devices
-    thread::sleep(Duration::from_secs(4));
-
-    // find the device we're interested in
-    let sensor = central
-        .peripherals()
-        .into_iter()
-        //.inspect(|p| println!("{:?}", p.properties().address))
-        .find(|p| p.properties().address == TARGET_DEV)
-        .unwrap();
-
-    // connect to the device
-    sensor.connect().unwrap();
-
-    // discover characteristics
-    sensor.discover_characteristics().unwrap();
-
-    // find the characteristic we want
-    let char_uuid = char_uuid_from(BASE_UUID, 42);
-    let chars = dbg!(sensor.characteristics());
-    dbg!(char_uuid);
-    let test_char = chars
-        .iter()
-        .inspect(|x| println!("{}", x.uuid))
-        .find(|c| c.uuid == char_uuid)
-        .unwrap();
-    dbg!(&test_char);
-    sensor.on_notification(Box::new(move |value| handle_notify(value, tx.clone())));
-    dbg!("trying to subscribe");
-    sensor.subscribe(&test_char).unwrap();
-    dbg!("subscribed succesfully");
-    let mut prev_numb = None;
-    let numb = loop {
-        dbg!("loop loop");
-        let recieved: Vec<u8> = rx.recv().unwrap();
-        let numb = read_be_u32(&mut &recieved[0..4]);
-
-        if let Some(prev_numb) = prev_numb {
-            if prev_numb + 1 != numb {
-                break numb;
+        let timeout = Some(Duration::from_secs(5));
+        let response_serial = self.connection.send_message(&mut connect, timeout).unwrap();
+        let msg = self.connection.wait_response(response_serial, timeout).unwrap();
+        
+        match msg.typ {
+            rustbus::message::MessageType::Reply => Ok(()),
+            rustbus::message::MessageType::Error => Err(Error::from(msg)),
+            _ => { 
+                let dbg_str = format!("Connect can only be awnserd 
+                    with Error or Reply however we got: {:?}", &msg);
+                dbg!(&dbg_str);
+                panic!();
             }
         }
-        prev_numb = Some(numb);
-    };
-    println!(
-        "Lost a packet as the expected number was: {:?} but we recieved: {:?}",
-        prev_numb, numb
-    );
+    }
+    
+    pub fn disconnect<T: Into<String>>(&mut self, adress: T) -> Result<(), Error> {
+        let adress =adress.into().replace(":","_");
+
+        let mut connect = MessageBuilder::new()
+            .call("Disconnect".into())
+            .at("org.bluez".into())
+            .on(format!("/org/bluez/hci{}/dev_{}",self.adapter_numb, adress))
+            .with_interface("org.bluez.Device1".into()) //is always Device1
+            .build();
+
+        let timeout = Some(Duration::from_secs(5));
+        let response_serial = self.connection.send_message(&mut connect, timeout).unwrap();
+        let msg = self.connection.wait_response(response_serial, timeout).unwrap();
+        
+        match msg.typ {
+            rustbus::message::MessageType::Reply => Ok(()),
+            rustbus::message::MessageType::Error => Err(Error::from(msg)),
+            _ => { 
+                let dbg_str = format!("Connect can only be awnserd 
+                    with Error or Reply however we got: {:?}", &msg);
+                dbg!(&dbg_str);
+                panic!();
+            }
+        }
+    }
+}
+
+
+use std::thread::sleep;
+fn main() {
+    let mut ble = BleBuilder::new().unwrap().build().unwrap();
+    ble.connect("C6:46:56:AC:2C:4C").unwrap();
+
+    sleep(Duration::from_secs(30));
+    ble.disconnect("C6:46:56:AC:2C:4C").unwrap();
+    //experiments::exp().unwrap();
 }
